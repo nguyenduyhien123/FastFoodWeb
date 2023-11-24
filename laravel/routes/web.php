1f7080ee2fa63e4839aa34dc13a3d9243a8dd5b1
mntrduz <144635533+mntrduz@users.noreply.github.com>
2023-11-24 19:20:27 +0700

    commit layout

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/single_product', function () {
+    return view('single_product');
+});
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
