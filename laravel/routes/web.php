6d127bbd984400251d63c25029f60792b4a76d10
mntrduz <144635533+mntrduz@users.noreply.github.com>
2023-11-25 07:47:07 +0700

    delete layout laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,17 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/single_product', function () {
-    return view('single_product');
-});
-
-Route::get('/products', function () {
-    return view('products');
-});
-
-Route::get('/about', function () {
-    return view('about');
+    return view('welcome');
 });
