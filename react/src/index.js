36fd6a6a94eadd6c5c20b357574fff179a751f55
mntrduz <144635533+mntrduz@users.noreply.github.com>
2023-12-13 23:26:58 +0700

    commit app

--- react/src/index.js
+++ react/src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./assets/css/fontawesome/fontawesome-5.15.4.css";
-import "./assets/css/website/style.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header/Header";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import Register from "./components/Register/Register";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Register />
-  </React.StrictMode>
-);
+import ReactDOM from "react-dom";
 
-reportWebVitals();
+//import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "remixicon/fonts/remixicon.css";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
