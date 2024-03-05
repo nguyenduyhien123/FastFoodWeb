9b5a800746fc0fb175ca7b744a24ddbfb4bb8122
mntrduz <144635533+mntrduz@users.noreply.github.com>
2024-03-05 07:03:03 +0700

    Update Header.jsx

--- react/src/components/headerFrontend/Header.jsx
+++ react/src/components/headerFrontend/Header.jsx
@@ -1,77 +1,81 @@
-import React, { useRef } from 'react';
-import { Link } from 'react-router-dom';
-import { Container } from 'reactstrap';
-import Cart from '../carts/Cart';
-import DropdownAvatar from '../dropdown_avatar/DropdownAvatar';
-import { ProductSearch } from '../product_search/ProductSearch';
-
+import React, { useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { Container } from 'reactstrap'
+import Cart from '../carts/Cart'
+import DropdownAvatar from '../dropdown_avatar/DropdownAvatar'
+import { ProductSearch } from '../product_search/ProductSearch'
+import "./header.scss"
 const navLinks = [
-  {
-    display: 'Trang chủ',
-    url: '/'
-  },
-  {
-    display: 'Thông tin',
-    url: '/about'
-  },
-  {
-    display: 'Thực đơn',
-    url: '#menu_pack'
-  },
-  {
-    display: 'Liên hệ',
-    url: '#footer_web'
-  },
-];
+       {
+              display: 'Trang chủ',
+              url: '/'
+       },
+       {
+              display: 'Thông tin',
+              url: '/about'
+       },
+       {
+              display: 'Thực đơn',
+              url: '#menu_pack'
+       },
+       {
+              display: 'Liên hệ',
+              url: '#footer_web'
+       },
+]
 
 const Header = () => {
-  const menuRef = useRef();
 
-  const menuToggle = () => menuRef.current.classList.toggle('active_menu');
+       const menuRef = useRef()
 
-  return (
-    <header className="header_web">
-      <Container>
-        <div className="navigation">
-          <div className="logo">
-            <Link to="/">
-              <h2 className='d-flex align-items-center gap-1'>
-                <span><i className="ri-restaurant-2-line"></i></span>{" "} Chef Food
-              </h2>
-            </Link>
-          </div>
-          <div className="nav_menu" ref={menuRef}>
-            <div className="nav_list_wrapper d-flex align-items-center gap-5">
-              <ul className="nav_list">
-                {navLinks.map((item, index) => (
-                  <li className="nav_item" key={index}><Link to={item.url} onClick={menuToggle}>{item.display}</Link></li>
-                ))}
-              </ul>
-              <div className="menu_right">
-                <ProductSearch />
-              </div>
-            </div>
-          </div>
-          <div className="d-flex list-action align-items-center gap-4">
-            <div className="avatar">
-              <DropdownAvatar />
-            </div>
-            <Link to={"/wishlist"}>
-              <button className="material-symbols-outlined favorite">
-                favorite
-              </button>
-            </Link>
-          </div>
-          <div>
-            <Cart />
-          </div>
-          <div className="mobile_menu">
-            <span><i className="ri-menu-line" onClick={menuToggle}></i></span>
-          </div>
-        </div>
-      </Container>
-    </header>
-  );
+       const menuToggle = () => menuRef.current.classList.toggle('active_menu')
+
+       return (
+              <header className="header_web">
+                     <Container>
+                            <div className="navigation">
+                                   <div className="logo">
+                                          <Link to="/">
+                                                 <h2 className='d-flex align-items-center gap-1'>
+                                                        <span><i class="ri-restaurant-2-line"></i></span>{" "} Chef Food
+                                                 </h2>
+                                          </Link>
+
+                                   </div>
+                                   <div className="nav_menu " ref={menuRef}>
+                                          <div className="nav_list_wrapper d-flex align-items-center gap-5">
+                                                 <ul className="nav_list">
+                                                        {
+                                                               navLinks.map((item, index) => (
+                                                                      <li className="nav_item" key={index}><Link to={item.url} onClick={menuToggle}>{item.display}</Link></li>
+                                                               ))
+                                                        }
+                                                 </ul>
+                                                 <div className="menu_right">
+                                                        <ProductSearch></ProductSearch>
+                                                 </div>
+                                          </div>
+                                   </div>
+                                   <div className="d-flex list-action align-items-center gap-4">
+                                          <div className="avatar">
+                                                 <DropdownAvatar></DropdownAvatar>
+                                          </div>
+                                          <Link to={"/wishlist"}>
+                                                 <button class="material-symbols-outlined favorite">
+                                                        favorite
+                                                 </button>
+                                          </Link>
+                                   </div>
+                                   <div>
+                                          <Cart />
+                                   </div>
+                                   <div className="mobile_menu">
+                                          <span><i class="ri-menu-line " onClick={menuToggle}></i></span>
+                                   </div>
+                            </div>
+                     </Container>
+              </header>
+       )
 }
 
-export default Header;
+export default Header
